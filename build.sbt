ca61c9245e51e56f473ccb837277cf9175d9dcf6
matthew-jp2525 <matthew.jp2525@gmail.com>
2023-01-14 07:36:45 +0900

    Test FileManager.

--- build.sbt
+++ build.sbt
@@ -8,5 +8,7 @@ lazy val root = project
 
     scalaVersion := scala3Version,
 
-    libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test
+    libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test,
+
+    libraryDependencies += "commons-io" % "commons-io" % "2.11.0" % Test
   )
---
+++ src/test/scala/com/matthewjp2525/simpledb/filemanager/FileManagerTest.scala
@@ -0,0 +1,36 @@
+package com.matthewjp2525.simpledb.filemanager
+
+import org.apache.commons.io.FileUtils
+
+import java.io.File
+import java.nio.file.Files
+
+class FileManagerTest extends munit.FunSuite {
+  val testDirs = FunFixture[File](
+    setup = { test =>
+      Files.createTempDirectory(test.name).toFile
+    },
+    teardown = { testDir =>
+      FileUtils.deleteDirectory(testDir)
+    }
+  )
+
+  testDirs.test("can read and write files") { testDir =>
+    val fileManager = FileManager(testDir, 400)
+
+    val blockId = BlockId("testfile", 2)
+    val page1 = Page(fileManager.blockSize)
+    val position1 = 88
+    page1.setString(position1, "あいうえお")
+    val size = Page.maxLength("あいうえお".length)
+    val position2 = position1 + size
+    page1.setInt(position2, 345)
+    fileManager.write(blockId, page1)
+
+    val page2 = Page(fileManager.blockSize)
+    fileManager.read(blockId, page2)
+
+    assertEquals(page2.getInt(position2), 345)
+    assertEquals(page2.getString(position1), "あいうえお")
+  }
+}
